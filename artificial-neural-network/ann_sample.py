d07f65fb71857703b014243b4aab30729db893b8
Ali Gholami <aligholami7596@gmail.com>
2017-11-14 14:27:20 +0330

    Calculate the adjustment value for the weights

--- artificial-neural-network/ann_sample.py
+++ artificial-neural-network/ann_sample.py
@@ -7,3 +7,51 @@
 # [] Created By : Ali Gholami (aligholami7596@gmail.com)
 # ========================================
 #
+import math
+from numpy import array, random, dot
+
+
+# The dataset from the perceptron (which it was failing at classification procedure)
+dataset = array([         (array([3, 4, 5]), 1),
+                    (array([3, 4, 6]), 0),
+                    (array([3, 3, 5]), 0),
+                    (array([3, 8, 5]), 0),
+                    (array([4, 4, 5]), 1),
+                    (array([1, 0, 5]), 1),
+                    (array([7, 4, 8]), 1),
+                    (array([1, 6, 4]), 0),
+                    (array([3, 4, 5]), 0),
+                    (array([4, 4, 6]), 1),
+                    (array([9, 5, 5]), 0)
+])
+
+# Define the initial values
+learning_rate = 0.02
+numOfIterations = 5000
+
+# Initial weights
+firstLayerWeights = random.rand(3, 2)
+secondLayerWeighs = random.rand(2, 1)
+
+# The beloved sigmoid function 
+def sigmoid(x):
+    return (1 / (1 + math.exp(-x)))
+
+# The sigmoid curve
+def sigmoidCurve(x):
+    return x * (1 - x)
+
+# ======================================== #
+# ======== Train The 2 Layer ANN ========= #
+# ======================================== #
+def trainNeuralNetwork(dataset, flw, slw, numOfIterations, learning_rate):
+    
+    for i in range(numOfIterations):
+        firstLayerOutputVector = sigmoid(dot(flw, dataset[:,0]))
+        secondLayerOutputVector = sigmoid(dot(slw, firstLayerOutputVector))
+
+        # Find the error
+        outputError = secondLayerOutputVector - dataset[:, 1]
+
+        # Calculate the adjustment value for the weights
+        weightAdjustmentValue = 
