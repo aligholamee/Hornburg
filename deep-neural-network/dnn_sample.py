0352dfeeb6420159333af26c635132201f4b4da9
Ali Gholami <aligholami7596@gmail.com>
2017-11-17 14:24:02 +0330

    Remove dnn for simplicity

--- deep-neural-network/dnn_sample.py
+++
@@ -1,9 +0,0 @@
-# Deep Neural Network
-# ========================================
-# [] File Name : dnn_sample.py
-#
-# [] Creation Date : December 2017
-#
-# [] Created By : Ali Gholami (aligholami7596@gmail.com)
-# ========================================
-#
